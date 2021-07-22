43d1dfa6883e8288ca8b46162bbfbc3284e6950f
Felix <felixdeveloperkettle@gmail.com>
2021-07-22 18:56:26 +0300

    A little fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//  There were paws Felix Only (felixdeveloperkettle@gmail.com)
+//  Author :    Felix Only (felixdeveloperkettle@gmail.com) 
+//  Date   :    22 July 2021 
+
 public class GameManager : MonoBehaviour
 {
-    public static bool GamePaused = true;
+	/* This is variable  will help with 
+	   understanding when the game are paused */
+	public static bool GamePaused = true;
+	
+	/* This is variable about collecting a point for a player */
     [SerializeField] private int _gamepoints;
-    public int GamePoints
+    
+	public int GamePoints
     {
         get
         {
@@ -24,16 +33,22 @@ public class GameManager : MonoBehaviour
             {
                 _gamepoints = 0;
             }
-
-
-
         }
     }
 
-    void Start() => GamePoints = 0;
+    void Start() 
+	{ 
+		// Nullify variable on starting a game 
+		GamePoints = 0;
+	}
 
+	/* Set variable */
     public void SetPause(bool Pause)
     {
         GamePaused = Pause;
     }
 }
+
+//  There were paws Felix Only (felixdeveloperkettle@gmail.com)
+//  Author :    Felix Only (felixdeveloperkettle@gmail.com) 
+//  Date   :    22 July 2021 
